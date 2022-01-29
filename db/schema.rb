a081dfb8ea456b1e4873de6c77c1f11e8d39df5a
yinghueichu <yinghuei.chu@essec.edu>
2022-01-29 13:25:38 +0100

    save

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_092247) do
+ActiveRecord::Schema.define(version: 2022_01_29_105903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_092247) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "type"
+    t.string "group_name"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,102 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+ProductTag.delete_all
+Product.delete_all
 User.delete_all
+
 
 puts 'Creating users...'
 User.create!([
   {name: "Manon", email: "manon@caddie.io", password: "manon@caddie.io"},
   {name: "Nicolas", email: "nicolas@caddie.io", password: "nicolas@caddie.io"},
-  {name: "Ying", email: "ying@caddie.io", password: "ying@caddie.io"},
+  {name: "Ying", email: "ying@caddie.io", password: "ying@caddie.io"}
 ])
 
 puts "#{User.count} users created"
+
+puts "Creating products.."
+
+Product.create!([
+  { name: "Peanut butter",
+    state: "to buy",
+    frequency: 28,
+    user_id: User.first.id
+  },
+  { name: "Pâte à tartiner",
+    state: "to buy",
+    frequency: 28,
+    user_id: User.first.id
+  },
+  { name: "Banana",
+    state: "to buy",
+    frequency: 7,
+    user_id: ( User.first.id + 1)
+  },
+  { name: "Soy milk",
+    state: "to buy",
+    frequency: 7,
+    user_id: ( User.first.id + 1)
+  },
+  { name: "Rice cake",
+    state: "to buy",
+    frequency: 14,
+    user_id: ( User.first.id + 2)
+  }
+])
+
+puts "#{Product.count} products created!"
+
+puts "Creating tags.."
+
+Tag.create!([
+  { name: "Terroir d'Avenir",
+    group_name: "merchant"
+  },
+  { name: "La Grange",
+    group_name: "merchant"
+  },
+  { name: "Zingam",
+    group_name: "merchant"
+  },
+  { name: "Keto",
+    group_name: "regime"
+  },
+  { name: "Fruit",
+    group_name: "section"
+  },
+  { name: "Snack",
+    group_name: "section"
+  },
+  { name: "Veggies",
+    group_name: "section"
+  },
+  { name: "Protein",
+    group_name: "section"
+  }
+])
+
+puts "#{Tag.count} tags created!"
+
+
+puts "Tagging product.."
+
+ProductTag.create!([
+  { product_id: Product.first.id,
+    tag_id: 1
+  },
+  { product_id: ( Product.first.id + 1),
+    tag_id: 1
+  },
+  { product_id: ( Product.first.id + 2),
+    tag_id: 2
+  },
+  { product_id: ( Product.first.id + 3),
+    tag_id: 2
+  },
+  { product_id: ( Product.first.id + 4),
+    tag_id: 3
+  }
+])
+
+puts "#{ProductTag.count} products tagged!"
