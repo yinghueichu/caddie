f3fb9e6296c33aee24f6e2c055a49f9cb8b8a464
manon rodriguez mate <ma.rodriguez.mate@gmail.com>
2022-02-03 18:58:26 +0100

    removed category from product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_194127) do
+ActiveRecord::Schema.define(version: 2022_02_03_175658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 2022_02_01_194127) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "aasm_state"
-    t.string "category"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
