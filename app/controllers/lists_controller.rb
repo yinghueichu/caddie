d92fe52878ae6e439cbb56acd88adb1b7b71acd2
yinghueichu <yinghuei.chu@essec.edu>
2022-02-03 21:02:56 +0100

    allow user to create a list and  mark a list of product as archive

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+
+  def create
+    @list = List.new(date: Date.today)
+    @list.user = current_user
+    @list.save!
+
+    @products_bought = Product.all.where(aasm_state: "bought")
+    @products_bought.each do |product|
+      product.archive!
+      product.save!
+      product_list = ProductList.new(product_id: product.id, list_id: @list.id)
+      product_list.save!
+    end
+
+    redirect_to products_path
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @product_lists = ProductList.all
+    @products_by_list = []
+    @product_lists.each do |product_list|
+      @products_by_list << Product.find(product_list.product_id) if product_list.list_id == @list.id
+    end
+    @products_by_list
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products, only: [:index, :create, :new, :edit, :update]
 
+  resources :lists, only: [:index, :create, :show]
+
 end
