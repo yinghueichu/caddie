3a0ac2750b3c68b2a4a97edb22d00d8384026521
Nicolas Deschamps <deschamps.nico92@gmail.com>
2022-02-05 14:29:48 +0100

    fsdf

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   def index
+    @tags = policy_scope(Tag).order(created_at: :desc)
     @tags = Tag.all
     @tags_category = Tag.all.where(group_name: "category")
   end
