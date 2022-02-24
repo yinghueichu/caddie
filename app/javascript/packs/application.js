92257a0b86a7298ad3f3a04cea6e34b4420aae0d
yinghueichu <yinghuei.chu@essec.edu>
2022-02-24 22:21:42 +0100

    save

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,25 +27,20 @@ import "extensions/turbolinks";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
-
 import 'materialize-css/dist/js/materialize';
 
 //Floating action button
-import { MDCRipple } from '@material/ripple';
+// import { MDCRipple } from '@material/ripple';
 
-var fab = document.querySelector('.mdc-fab');
+// var fab = document.querySelector('.mdc-fab');
 
-if (fab) {
-  const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
-  document.addEventListener('turbolinks:load', function () {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, 'top');
-  });
-}
+// if (fab) {
+//   const fabRipple = new MDCRipple(document.querySelector('.mdc-fab'));
+//   document.addEventListener('turbolinks:load', function () {
+//     var elems = document.querySelectorAll('.fixed-action-btn');
+//     var instances = M.FloatingActionButton.init(elems, 'top');
+//   });
+// }
 
 
 //file picker
@@ -53,7 +48,7 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
-document.addEventListener('turbolinks:load', function () {
+document.addEventListener('turbolinks:load', () => {
   console.log("turbolinks ready!");
 
   const btnAll = document.querySelector('#btnAll');
@@ -109,70 +104,68 @@ document.addEventListener('turbolinks:load', function () {
     })
   }
 
-
   const realFileBtn = document.querySelector('#product_photo');
   const previewContainer = document.querySelector('#imagePreview');
-  const previewDefaultText = previewContainer.querySelector('.image-preview__text');
-  const previewImage = previewContainer.querySelector('.image-preview__image');
+  if (previewContainer) {
+    const previewDefaultText = previewContainer.querySelector('.image-preview__text');
+    const previewImage = previewContainer.querySelector('.image-preview__image');
 
-  if (previewImage) {
-    previewContainer.addEventListener('click', function () {
-      realFileBtn.click();
-    })
+    if (previewImage) {
+      previewContainer.addEventListener('click', function () {
+        realFileBtn.click();
+      })
 
-    realFileBtn.addEventListener("change", function () {
-      const file = realFileBtn.files[0];
+      realFileBtn.addEventListener("change", function () {
+        const file = realFileBtn.files[0];
 
-      if (file) {
-        const reader = new FileReader();
-        previewDefaultText.style.display = 'none';
-        previewImage.style.display = 'block';
-        reader.addEventListener('load', function () {
-          previewImage.setAttribute('src', this.result);
-        });
-        reader.readAsDataURL(file);
+        if (file) {
+          const reader = new FileReader();
+          previewDefaultText.style.display = 'none';
+          previewImage.style.display = 'block';
+          reader.addEventListener('load', function () {
+            previewImage.setAttribute('src', this.result);
+          });
+          reader.readAsDataURL(file);
 
-      } else {
-        previewImage.style.display = 'none';
-      }
-    });
-  }
-
-
-
-
+        } else {
+          previewImage.style.display = 'none';
+        }
+      });
+    }
+  };
 });
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     console.log("DOMContentLoaded");
+//     const btnAll = document.querySelector('#btnAll');
+//     const btnToBuy = document.querySelector('#btnToBuy');
+//     const btnBought = document.querySelector('#btnBought');
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log("DOMContentLoaded");
-  const btnAll = document.querySelector('#btnAll');
-  const btnToBuy = document.querySelector('#btnToBuy');
-  const btnBought = document.querySelector('#btnBought');
-
-  const navAll = document.querySelector('#navAll');
-  const navToBuy = document.querySelector('#navToBuy');
-  const navBought = document.querySelector('#navBought');
+//     const navAll = document.querySelector('#navAll');
+//     const navToBuy = document.querySelector('#navToBuy');
+//     const navBought = document.querySelector('#navBought');
 
 
-  if (btnAll) {
-    btnAll.focus();
-    btnAll.addEventListener("click", function () {
-      navAll.style.display = "block";
-      navToBuy.style.display = "none";
-      navBought.style.display = "none";
-    })
+//     if (btnAll) {
+//       btnAll.focus();
+//       btnAll.addEventListener("click", function () {
+//         navAll.style.display = "block";
+//         navToBuy.style.display = "none";
+//         navBought.style.display = "none";
+//       })
 
-    btnToBuy.addEventListener("click", function () {
-      navAll.style.display = "none";
-      navToBuy.style.display = "block";
-      navBought.style.display = "none";
-    })
+//       btnToBuy.addEventListener("click", function () {
+//         navAll.style.display = "none";
+//         navToBuy.style.display = "block";
+//         navBought.style.display = "none";
+//       })
 
-    btnBought.addEventListener("click", function () {
-      navAll.style.display = "none";
-      navToBuy.style.display = "none";
-      navBought.style.display = "block";
-    })
-  }
-});
+//       btnBought.addEventListener("click", function () {
+//         navAll.style.display = "none";
+//         navToBuy.style.display = "none";
+//         navBought.style.display = "block";
+//       })
+//     }
+
+//   }
+// });
